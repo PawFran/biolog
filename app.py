29cf81700bbea3aab5c64c1c54a212c4903b71cf
pawel.franczak <pawel.fran@gmail.com>
2023-10-07 17:17:37 +0200

    handle multiple files (without downloading csv)

--- app.py
+++ app.py
@@ -5,53 +5,79 @@ from entry import *
 
 st.title('Fitting Sigmoid')
 
-uploaded_file = st.file_uploader('Upload csv file', type='csv')
+uploaded_files = st.file_uploader('Upload csv file', type='csv', accept_multiple_files=True)
+file_names = [f.name.split('.')[0] for f in uploaded_files]
 
-if uploaded_file is not None:
-    df_raw = pd.read_csv(uploaded_file, sep=';')
+if len(uploaded_files) > 0:
+    df_raw_lst = []
+    intermediate_lst = []
+    final_lst = []
+    aggregated_lst = []
+    final_aggregated_lst = []
 
-    intermediate = pre_process(df_raw)
-    final = fit_model(intermediate)
-    aggregated = mean_each_trial(intermediate)
-    final_aggregated = fit_model(aggregated)
+    for uploaded_file in uploaded_files:
+        df_raw = pd.read_csv(uploaded_file, sep=';')
 
-    aggregated_joined_with_model = aggregated.merge(final_aggregated, on='trial')
+        intermediate = pre_process(df_raw)
+        final = fit_model(intermediate)
+        aggregated = mean_each_trial(intermediate)
+        final_aggregated = fit_model(aggregated)
+
+        # aggregated_joined_with_model = aggregated.merge(final_aggregated, on='trial')
+
+        df_raw_lst.append(df_raw)
+        intermediate_lst.append(intermediate)
+        final_lst.append(final)
+        aggregated_lst.append(aggregated)
+        final_aggregated_lst.append(final_aggregated)
 
     if st.checkbox('Show raw data'):
         st.subheader('raw data')
-        st.write(df_raw)
+        for df_raw, file_name in zip(df_raw_lst, file_names):
+            st.write(file_name)
+            st.write(df_raw)
 
     if st.checkbox('Show processed data'):
         st.subheader('preprocessed data')
-        st.write(intermediate)
+        for intermediate, file_name in zip(intermediate_lst, file_names):
+            st.write(file_name)
+            st.write(intermediate)
 
         st.subheader('model fitted per substrate')
-        st.write(final)
+        for final, file_name in zip(final_lst, file_names):
+            st.write(file_name)
+            st.write(final)
 
         st.subheader('mean each trial')
-        st.write(aggregated)
+        for aggregated, file_name in zip(aggregated_lst, file_names):
+            st.write(file_name)
+            st.write(aggregated)
 
         st.subheader('model fitted per mean')
-        st.write(final_aggregated)
+        for final_aggregated, file_name in zip(final_aggregated_lst, file_names):
+            st.write(file_name)
+            st.write(final_aggregated)
 
     if st.button('Show plot for aggregated data'):
-        for i in aggregated.index:
-            to_plot = aggregated[aggregated.index == i].transpose()
-            to_plot.index = to_plot.index.astype(float)
+        for aggregated, file_name in zip(aggregated_lst, file_names):
+            st.write(file_name)
+            for i in aggregated.index:
+                to_plot = aggregated[aggregated.index == i].transpose()
+                to_plot.index = to_plot.index.astype(float)
 
-            hours = to_plot.index.values
-            xs = np.linspace(min(hours), max(hours))
-            ys_measured = to_plot.values.reshape(10)
+                hours = to_plot.index.values
+                xs = np.linspace(min(hours), max(hours))
+                ys_measured = to_plot.values.reshape(len(to_plot))
 
-            params = final_aggregated.iloc[i-1]
-            L, x0, k, b = params['L'], params['x0'], params['k'], params['b']
-            ys_fitted = sigmoid(xs, L, x0, k, b)
+                params = final_aggregated.iloc[i - 1]
+                L, x0, k, b = params['L'], params['x0'], params['k'], params['b']
+                ys_fitted = sigmoid(xs, L, x0, k, b)
 
-            fig = go.Figure()
-            fig.add_trace(trace=go.Scatter(x=hours, y=ys_measured, mode='markers', name='measured'))
-            fig.add_trace(go.Scatter(x=xs, y=ys_fitted, mode='lines', name='fitted'))
+                fig = go.Figure(layout_title_text=f'trial {i}')
+                fig.add_trace(trace=go.Scatter(x=hours, y=ys_measured, mode='markers', name='measured'))
+                fig.add_trace(go.Scatter(x=xs, y=ys_fitted, mode='lines', name='fitted'))
 
-            st.plotly_chart(fig)
+                st.plotly_chart(fig)
 
 
     @st.cache_data
@@ -61,9 +87,9 @@ if uploaded_file is not None:
 
     csv = convert_df(final_aggregated)
 
-    st.download_button(
-        label="Download data as CSV",
-        data=csv,
-        file_name=uploaded_file.name,
-        mime='text/csv',
-    )
+    # st.download_button(
+    #     label="Download data as CSV",
+    #     data=csv,
+    #     file_name=uploaded_file.name,
+    #     mime='text/csv',
+    # )
--- model_fitting.py
+++ model_fitting.py
@@ -20,24 +20,8 @@ def fit_model(df):
         try:
             params, covariance = curve_fit(sigmoid, hours, ydata, p0, method='lm')
         except RuntimeError:
-            params = [0, 0, 0]
+            print('Runtime error during curve fitting. 0s will be used as fallback params')
+            params = [0, 0, 0, 0]
         return pd.Series(np.round(params, 4), index=['L', 'x0', 'k', 'b'])
 
     return df.apply(fit_curve, axis=1)
-
-# def fit_model(df):
-#     def sigmoid(x, a, b, k):
-#         y = a / (1 + b * np.exp(-k * x))
-#         return y
-#
-#     hours = df.columns.astype(np.float64)
-#
-#     def fit_curve(ydata):
-#         ydata = np.array(ydata).astype(np.float64)
-#         try:
-#             params, covariance = curve_fit(sigmoid, hours, ydata, method='lm')
-#         except RuntimeError:
-#             params = [0, 0, 0]
-#         return pd.Series(np.round(params, 4), index=['a', 'b', 'k'])
-#
-#     return df.apply(fit_curve, axis=1)
